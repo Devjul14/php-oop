2698c90ce2895e664370e8016a1a37e8c2bcbf81
julia eka <eka47577@gmail.com>
2021-09-28 21:54:48 +0700

    new

--- datetimie.php
+++ datetimie.php
@@ -6,7 +6,6 @@ $dateTime->setTimezone(new DateTimeZone("Asia/Jakarta"));
 $now = new DateTime();
 $now->setTimezone(new DateTimeZone("Asia/Jakarta"));
 $date = $now->format("H:i:s");
-
 echo "waktu saat ini $date";
 
 // 
